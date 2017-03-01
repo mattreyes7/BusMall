f3240772d579c30b7d9d0fc91a106a2b3c47ece4
mattreyes7 <mattreyes615@gmail.com>
2017-02-28 21:07:26 -0800

    refactored entire project

--- app.js
+++ app.js
@@ -1,112 +1,110 @@
-// var products = ['bag',
-// 'banana',
-// 'bathroom',
-// 'boots',
-// 'breakfast',
-// 'bubblegum',
-// 'chair', 'cthulu',
-// 'dog-duck',
-// 'dragon',
-// 'pen',
-// 'pet-sweep',
-// 'scissors',
-// 'shark',
-// 'sweep',
-// 'tauntaun',
-// 'unicorn',
-// 'usb',
-// 'water-can',
-// 'wine-glass'];
 
-// var productArray = [];
-//
-// function Product(name, path) {
-//   this.name = name;
-//   this.path = path;
-// }
-//
-// var bag = new Product('bag','assets/bag.jpg');
-// var banana = new Product('banana','assets/banana.jpg');
-// var boots = new Product('boots','assets/boots.jpg');
-// var breakfast = new Product('breakfast','assets/breakfast.jpg');
-// var bubblegum = new Product('bubblegum','assets/bubblegum.jpg');
-// var chair = new Product('chair','assets/chair.jpg');
-// var cthulu = new Product('cthulu', 'assets/cthulu.jpg');
-// var dogDuck= new Product('dog-duck', 'assets/dog-duck.jpg');
-// var dragon= new Product('dragon', 'assets/dragon.jpg');
-// var pen= new Product('pen', 'assets/pen.jpg');
-// var petSweep= new Product('pet-sweep', 'assets/pet-sweep.jpg');
-// var scissors= new Product('scissors', 'assets/scissors.jpg');
-// var shark= new Product('shark', 'assets/shark.jpg');
-// var sweep= new Product('sweep', 'assets/sweep.jpg');
-// var tauntaun= new Product('tauntaun', 'assets/tauntaun.jpg');
-// var unicorn= new Product('unicorn', 'assets/unicorn.jpg');
-// var usb= new Product('usb', 'assets/usb.jpg');
-// var waterCan= new Product('water-can', 'assets/water-can.jpg');
-// var wineGlass= new Product('wine-glass', 'assets/wine-glass.jpg');
+var productImageNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var productsArray = [];
 
-var productPath = [ 'assets/bag.jpg',
-'assets/banana.jpg',
-'assets/bathroom.jpg',
-'assets/boots.jpg',
-'assets/breakfast.jpg',
-'assets/bubblegum.jpg',
-'assets/chair.jpg',
-'assets/cthulhu.jpg',
-'assets/dog-duck.jpg',
-'assets/dragon.jpg',
-'assets/pen.jpg',
-'assets/pet-sweep.jpg',
-'assets/scissors.jpg',
-'assets/shark.jpg',
-'assets/sweep.jpg',
-'assets/tauntaun.jpg',
-'assets/unicorn.jpg',
-'assets/usb.jpg',
-'assets/water-can.jpg',
-'assets/wine-glass.jpg'];
+function Products(name) {
+  this.name = name;
+  this.path = './assets/' + name + '.jpg';
+  this.votes = 0;
+  productsArray.push(this)
+
+}
+// builds products with iffe
+(function() {
+  for(var i = 0; i < productImageNames.length; i++) {
+    new Products(productImageNames[i]);
+  }
+})();
+
+var tracker = {
+  imageOneEl: document.getElementById('imageOne'),
+  imageTwoEl: document.getElementById('imageTwo'),
+  imageThreeEl: document.getElementById('imageThree'),
+  showResultsEl:document.getElementById('show-results'),
+  resultsEl: document.getElementById('results'),
+  imageContainerEl: document.getElementById('image-container'),
+  imageObjOne: null,
+  imageObjTwo: null,
+  imageObjThree: null,
+  clicks: 1,
 
 
-var randomImage = function () {
-  return Math.floor(Math.random() * (productPath.length));
+  getRandomIndex: function() {
+    return Math.floor(Math.random() * productsArray.length);
+  },
+
+  // function that displays 3 random non repeating images
+  displayImages: function() {
+    this.imageObjOne = productsArray[this.getRandomIndex()];
+    this.imageObjTwo = productsArray[this.getRandomIndex()];
+    this.imageObjThree = productsArray[this.getRandomIndex()];
+
+    if(this.imageObjOne === this.imageObjTwo || this.imageObjOne === this.imageObjThree || this.imageObjTwo === this.imageObjThree){
+      this.displayImages();
+    }
+    this.imageOneEl.src = this.imageObjOne.path;
+    this.imageOneEl.id = this.imageObjOne.name;
+
+    this.imageTwoEl.src = this.imageObjTwo.path;
+    this.imageTwoEl.id = this.imageObjTwo.name;
+
+    this.imageThreeEl.src = this.imageObjThree.path;
+    this.imageThreeEl.id = this.imageObjThree.name;
+
+  },
+
+  checkClicks: function() {
+    console.log(this.clicks);
+    if(this.clicks > 14) {
+      this.imageContainerEl.removeEventListener('click', this.clickHandler);
+      this.showResultsEl.addEventListener('click', function(e) {
+        e.preventDefault();
+        tracker.renderResults();
+      });
+    }
+
+  },
+
+  clickHandler: function(e) {
+    tracker.checkClicks();
+    if (
+      e.target.id === tracker.imageObjOne.name ||
+      e.target.id ===  tracker.imageObjTwo.name ||
+      e.target.id === tracker.imageObjThree.name
+    ) {
+      tracker.clicks++;
+      tracker.tallyVotes(e.target.id);
+      tracker.displayImages();
+    }
+
+  },
+
+  tallyVotes: function(elId) {
+    for(var i in productsArray) {
+      if(elId === productsArray[i].name) {
+        productsArray[i].votes +=1;
+        console.log(productsArray[i]);
+        break;
+      }
+    }
+  },
+
+  renderResults: function() {
+    // create an UL
+    // create each of the LIs and add content
+    // append each LI to the UL
+    // append the Ul to the document
+    var ulEl = document.createElement('ul');
+
+    for(var i in productsArray) {
+      var liEl = document.createElement('li');
+      liEl.textContent = productsArray[i].name + ': ' + productsArray[i].votes;
+      ulEl.appendChild(liEl);
+    }
+
+    this.resultsEl.appendChild(ulEl);
+  }
 };
 
-function showImages() {
-  var imageOneBox = document.getElementById('imageOne');
-  var showImageOne = document.createElement('img');
-  showImageOne.src = productPath[randomImage()];
-  imageOneBox.appendChild(showImageOne);
-
-
-  var imageTwoBox = document.getElementById('imageTwo');
-  var showImageTwo = document.createElement('img');
-  showImageTwo.src = productPath[randomImage()];
-  imageTwoBox.appendChild(showImageTwo);
-
-
-  var imageThreeBox = document.getElementById('imageThree');
-  var showImageThree = document.createElement('img');
-  showImageThree.src = productPath[randomImage()];
-  imageThreeBox.appendChild(showImageThree);
-}
-
-var imageContainer = document.getElementById('image-container');
-imageContainer.addEventListener('click', function(){
-  showImages()
-// refreshes pictures
-var refreshImages = document.getElementsByTagName('img');
-for (var i = 0; i < refreshImages.length-1; i++) {
-  refreshImages[0].parentNode.removeChild(refreshImages[0]);
-}
-});
-
-// function tracker() {
-//   var imageContainer = document.getElementById('image-container');
-//   imageContainer.addEventListener('click', function(){
-//     showImages()
-//   });
-//
-//   var refresh = document.getElementById('image-container');
-//   while(productPath.length > 0) {
-//     productPath[0].parentNode.removeChild(productPath[0]);
-//   }
+tracker.imageContainerEl.addEventListener('click', tracker.clickHandler);
+tracker.displayImages();
